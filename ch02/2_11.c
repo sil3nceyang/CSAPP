2fa98135dd6d50e9d2135aab0dd3e6cde34d0ed6
sil3nceyang <109602915+sil3nceyang@users.noreply.github.com>
2024-02-13 11:01:39 +0100

    2_11.c

--- ch02/2_11.c
+++ ch02/2_11.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+void inplace_swap(int *x, int *y);
+void reverse_array(int a[], int cnt);
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    reverse_array(arr, size);
+
+    printf("Reversed array: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+void inplace_swap(int *x, int *y)
+{
+    *y = *x ^ *y;
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+}
+
+void reverse_array(int a[], int cnt)
+{
+    int first, last;
+    for (first = 0, last = cnt - 1;
+         first < last; first++, last--)
+         inplace_swap(&a[first], &a[last]);
+}
